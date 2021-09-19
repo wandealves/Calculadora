8ff063f3ef58d484c54512685082cfb0de7190a7
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2021-09-19 10:38:22 -0300

    feat: criação da implementaçaõ dos calculos primo

---
+++ src/Calculadora.Dominio/Servicos/PrimoService.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+using Calculadora.Dominio.Interfaces;
+
+namespace Calculadora.Dominio.Servicos
+{
+    public class PrimoService: IPrimoService
+    {
+        private readonly IMemorizacao<string, long> _memorizacao;
+
+        public PrimoService(IMemorizacao<string, long> memorizacao) 
+        {
+            _memorizacao = memorizacao;
+        }
+
+        public bool EhPrimo(long numero)
+        {
+            var valoresPrimos = _memorizacao.ObterValores(nameof(EhPrimo));
+
+            if (valoresPrimos != null && valoresPrimos.Any(x => x == numero))
+            {
+                return true;
+            }
+
+            var ehPrimo = numero != 1;
+
+            for (int i = 2; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    ehPrimo = false;
+                    break;
+                }
+            }
+
+            if (ehPrimo)
+                _memorizacao.AdicionarValor(nameof(EhPrimo), numero);
+            return ehPrimo;
+        }
+    }
+}
