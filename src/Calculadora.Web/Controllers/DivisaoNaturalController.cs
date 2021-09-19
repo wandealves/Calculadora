6cd98704bfb9aecf551c26714e78331637711c3d
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2021-09-19 12:21:37 -0300

    feat: criação controle

---
+++ src/Calculadora.Web/Controllers/DivisaoNaturalController.cs
@@ -0,0 +1,28 @@
+﻿using Calculadora.Dominio.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculadora.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DivisaoNaturalController : ControllerBase
+    {
+        private readonly IDivisorNaturalService _divisorNaturalService;
+
+        public DivisaoNaturalController(IDivisorNaturalService divisorNaturalService)
+        {
+            _divisorNaturalService = divisorNaturalService;
+        }
+
+        [HttpGet]
+        public IActionResult Get(long numero)
+        {
+            var result = _divisorNaturalService.DivisoresNaturais(numero);
+
+            if (result == null) return BadRequest(new { Error = "Informe um número maior que zero" });
+
+            return Ok(result);
+        }
+    }
+}
