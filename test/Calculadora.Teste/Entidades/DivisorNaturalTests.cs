af699cd136e236ead5db7670d69e3f233b9b236b
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2021-09-19 11:54:55 -0300

    feat: criação dos testes

---
+++ test/Calculadora.Teste/Entidades/DivisorNaturalTests.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+using Calculadora.Dominio.Entidades;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculadora.Teste.Entidades
+{
+    [TestClass]
+    public class DivisorNaturalTests
+    {
+        [TestMethod]
+        [TestCategory("Dominio")]
+        public void Dado_um_numero_deve_calcular_os_fatores_primos()
+        {
+            var numero = 45;
+            var fakeDivisores = new List<long>() { 1, 3, 5 };
+
+            var divisorNatural = new DivisorNatural();
+            var fatoresPrimos = divisorNatural.CalcularFatorPrimo(numero).ToList();
+
+            var atual = fatoresPrimos.Distinct().OrderBy(x => x).ToList();
+
+            CollectionAssert.AreEqual(fakeDivisores, atual);
+        }
+
+        [TestMethod]
+        [TestCategory("Dominio")]
+        public void Dado_um_numero_deve_calcular_os_divisores_naturais()
+        {
+            var numero = 45;
+            var fakeDivisores = new List<long>() { 1, 3, 5, 9, 15, 45 };
+
+            var divisorNatural = new DivisorNatural();
+            var fatoresPrimos = divisorNatural.CalcularFatorPrimo(numero).ToList();
+            var divisores = divisorNatural.CalcularDivisores(fatoresPrimos);
+
+            var atual = divisores.Distinct().OrderBy(x => x).ToList();
+
+            CollectionAssert.AreEqual(fakeDivisores, atual);
+        }
+    }
+}
