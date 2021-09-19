b1c65a42c1f5d6b465a92292936ead7ddc790d24
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2021-09-19 11:53:52 -0300

    refactor: remoção do calculo

--- src/Calculadora.Dominio/Servicos/DivisorNaturalService.cs
+++ src/Calculadora.Dominio/Servicos/DivisorNaturalService.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
 using Calculadora.Dominio.Entidades;
 using Calculadora.Dominio.Interfaces;
@@ -8,11 +7,9 @@ namespace Calculadora.Dominio.Servicos
 {
     public class DivisorNaturalService : IDivisorNaturalService
     {
-        private readonly IPrimoService _primoService;
-        private readonly IMemorizacao<string, long> _memorizacao;
-        public DivisorNaturalService(IPrimoService primoService, IMemorizacao<string, long> memorizacao)
+        private readonly IMemorizacao<long, DivisorNatural> _memorizacao;
+        public DivisorNaturalService(IMemorizacao<long, DivisorNatural> memorizacao)
         {
-            _primoService = primoService;
             _memorizacao = memorizacao;
         }
 
@@ -20,53 +17,21 @@ namespace Calculadora.Dominio.Servicos
         {
             if (numero <= 0) return default(DivisorNatural);
 
+            var result = _memorizacao.Obter(numero);
+
+            if (result != null) return result;
+
             var divisorNatural = new DivisorNatural();
 
-            var fatoresPrimos = CalcularFatorPrimo(numero, numero);
+            var fatoresPrimos = divisorNatural.CalcularFatorPrimo(numero).ToList();
             var divisores = divisorNatural.CalcularDivisores(fatoresPrimos);
 
             divisorNatural.AddDivisores(divisores.Distinct().OrderBy(x => x).ToList());
             divisorNatural.AddPrimos(fatoresPrimos.Distinct().OrderBy(x => x).ToList());
 
+            _memorizacao.Adicionar(numero, divisorNatural);
+
             return divisorNatural;
-        }
-
-        private List<long> CalcularFatorPrimo(long valorOriginal, long numero, long fator = 2, List<long> fatores = null)
-        {
-            if (fatores == null)
-            {
-                fatores = new List<long>();
-                fatores.Add(1);
-            }
-
-            if (valorOriginal == numero)
-            {
-                var listaNoDicionario = _memorizacao.Obter(nameof(CalcularFatorPrimo), numero.ToString());
-
-                if (listaNoDicionario?.Any() == true) return listaNoDicionario.ToList();
-            }
-
-            var divisor = numero;
-
-            var resto = numero % fator;
-            var ehPrimo = _primoService.EhPrimo(fator);
-
-            if (resto == 0 && ehPrimo)
-            {
-                divisor = numero / fator;
-                fatores.Add(fator);
-            }
-
-            if (resto != 0)
-                fator += 1;
-
-            if (divisor == 1)
-            {
-                _memorizacao.Adicionar(nameof(CalcularFatorPrimo), valorOriginal.ToString(), fatores);
-                return fatores;
-            }
-            else
-                return CalcularFatorPrimo(valorOriginal, divisor, fator, fatores);
         }
     }
 }
