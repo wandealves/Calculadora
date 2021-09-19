aecf8e39c46c2d994cc3fc265ad467445cdc249e
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2021-09-19 11:50:17 -0300

    refactor: alterações dos métodos do contrato

--- src/Calculadora.Dominio/Interfaces/IMemorizacao.cs
+++ src/Calculadora.Dominio/Interfaces/IMemorizacao.cs
@@ -1,15 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace Calculadora.Dominio.Interfaces
+﻿namespace Calculadora.Dominio.Interfaces
 {
     public interface IMemorizacao<T, U>
     {
         void Adicionar(T chave, U valor);
-        void Adicionar(T chave, List<U> valores);
-        void Adicionar(T chaveExterna, T chaveInterna, U valor);
-        void Adicionar(T chaveExterna, T chaveInterna, IList<U> valores);
-        IList<U> Obter(T chave);
-        U ObterUnico(T chave);
-        IList<U> Obter(T chaveExterna, T chaveInterna);
+        U Obter(T chave);
     }
 }
