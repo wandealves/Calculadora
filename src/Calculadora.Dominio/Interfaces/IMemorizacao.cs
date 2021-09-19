47bf857bd9f34af33b98d22976e15d9aeba47f9a
Wanderson Rodrigues <wanderson.rodrigues@venturisfinancial.com>
2021-09-19 10:36:38 -0300

    feat: criação do contrato para a memorização do dados

---
+++ src/Calculadora.Dominio/Interfaces/IMemorizacao.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Calculadora.Dominio.Interfaces
+{
+    public interface IMemorizacao<T, U>
+    {
+        void AdicionarValor(T chave, U valor);
+        void AdicionarValores(T chave, List<U> valores);
+        void AdicionarValorNoDicionario(T chaveExterna, T chaveInterna, U valor);
+        void AdicionarValoresNoDicionario(T chaveExterna, T chaveInterna, IList<U> valores);
+        IList<U> ObterValores(T chave);
+        U ObterValor(T chave);
+        IList<U> ObterValoresdoDicionario(T chaveExterna, T chaveInterna);
+    }
+}
